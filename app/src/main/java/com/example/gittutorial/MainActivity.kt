e8e316be30e81abdbd6071011f7bdab1fb9a20ed
TheLittleBug404 <ricardojaureguilima@gmail.com>
2023-07-26 23:06:20 -0400

    agregamos la linea println en el mainActivity

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Esto es para ver si se hacen cambios en el commit")
     }
 }
